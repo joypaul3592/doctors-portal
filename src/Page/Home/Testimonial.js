c5ff6d3246a716fcb8e0e6185e4adf59c7696c1e
Joy Paul <joypaul3592@gmail.com>
2022-05-11 00:23:28 +0600

    testimonit update

--- src/Page/Home/Testimonial.js
+++ src/Page/Home/Testimonial.js
@@ -50,7 +50,7 @@ const Testimonial = () => {
             <h4 className=' text-primary text-xl font-sans font-bold'>Testimonial</h4>
             <h2 className='text-accent font-sans text-4xl '>What Our Patients Says</h2>
 
-            <div className="my-8">
+            <div className="my-8 grid grid-cols-1 lg:grid-cols-3 gap-5">
                 {
                     reviews.map(review => <TestimonialCard key={review._id} review={review}></TestimonialCard>)
                 }
--- src/Page/Home/TestimonialCard.js
+++ src/Page/Home/TestimonialCard.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
-const TestimonialCard = () => {
+const TestimonialCard = ({ review }) => {
     return (
-        <div>
-            <h1>Its card</h1>
+        <div class="card card-compact w-96 bg-base-100 shadow-xl p-5">
+            <div className='mb-8'>
+                <p>{review.review}</p>
+            </div>
+            <div className='flex items-center'>
+                <img className='w-[75px] h-[75px] mr-5' src={review.img} alt="people" />
+                <div>
+                    <h1>{review.name}</h1>
+                    <h4>{review.country}</h4>
+                </div>
+            </div>
         </div>
     );
 };
