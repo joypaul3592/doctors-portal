8febfed38b61fb7666b4e06f11adbf2950121173
Joy Paul <joypaul3592@gmail.com>
2022-05-11 00:05:21 +0600

    adding booking

---
+++ src/Page/Home/Testimonial.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Testimonial = () => {
+    return (
+        <div>
+            <h4 className=' text-primary text-xl font-sans font-bold'>Testimonial</h4>
+            <h2 className='text-accent font-sans text-4xl '>What Our Patients Says</h2>
+        </div>
+    );
+};
+
+export default Testimonial;
