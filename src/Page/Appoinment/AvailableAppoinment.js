378c7ab76a292a4fd32e6a14dd6b0c57f13e3fb7
Joy Paul <joypaul3592@gmail.com>
2022-05-12 17:04:57 +0600

    adding update

--- src/Page/Appoinment/AvailableAppoinment.js
+++ src/Page/Appoinment/AvailableAppoinment.js
@@ -8,7 +8,7 @@ const AvailableAppoinment = ({ date }) => {
     const [treatment, setTreatment] = useState(null)
 
     useEffect(() => {
-        fetch('services.json')
+        fetch('http://localhost:5000/services')
             .then(res => res.json())
             .then(data => setServices(data))
 
